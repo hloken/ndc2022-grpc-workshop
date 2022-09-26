48ed18df17235df658bf922e1ac85aaf195ee498
Hans Løken <hans@ebamse.net>
2022-09-26 13:30:55 +0200

    Example of integration-test

---
+++ test/Ingredients.Tests/IngredientsApplicationFactory.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Net.Client;
+using Ingredients.Protos;
+using Ingredients.Services;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Ingredients.Tests;
+
+public class IngredientsApplicationFactory : WebApplicationFactory<IngredientsImpl>
+{
+    public IngredientsService.IngredientsServiceClient CreateGrpcClient()
+    {
+        var httpClient = CreateClient();
+
+        var channel = GrpcChannel.ForAddress(httpClient.BaseAddress!, new GrpcChannelOptions
+        {
+            HttpClient = httpClient
+        });
+
+        return new IngredientsService.IngredientsServiceClient(channel);
+    }
+}
---
+++ test/Ingredients.Tests/ToppingsTests.cs
@@ -0,0 +1,24 @@
+using Grpc.Net.ClientFactory;
+using Ingredients.Protos;
+
+namespace Ingredients.Tests;
+
+public class ToppingsTests: IClassFixture<IngredientsApplicationFactory>
+{
+    private readonly IngredientsApplicationFactory _factory;
+
+
+    public ToppingsTests(IngredientsApplicationFactory factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task Test1()
+    {
+        var client = _factory.CreateGrpcClient();
+        var response = await client.GetToppingsAsync((new GetToppingsRequest()));
+        
+        Assert.NotEmpty(response.Toppings);
+    }
+}
