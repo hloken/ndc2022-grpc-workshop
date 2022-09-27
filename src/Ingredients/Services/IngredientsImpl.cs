8cf64afd5b6d767df4d17cdeb89ebd3ca1715d32
Hans Løken <hans@ebamse.net>
2022-09-27 16:31:15 +0200

    Added Kubernetes. Crashes

--- src/Ingredients/Services/IngredientsImpl.cs
+++ src/Ingredients/Services/IngredientsImpl.cs
@@ -1,4 +1,5 @@
-﻿using Grpc.Core;
+﻿using System.Diagnostics;
+using Grpc.Core;
 using Ingredients.Data;
 using Ingredients.Protos;
 
@@ -8,15 +9,18 @@ public class IngredientsImpl : IngredientsService.IngredientsServiceBase
 {
     private readonly IToppingData _toppingData;
     private readonly ICrustData _crustData;
-    
-    public IngredientsImpl(IToppingData toppingData, ICrustData crustData)
+    private readonly ILogger<IngredientsImpl> _logger;
+
+    public IngredientsImpl(IToppingData toppingData, ICrustData crustData, ILogger<IngredientsImpl> logger)
     {
         _toppingData = toppingData;
         _crustData = crustData;
+        _logger = logger;
     }
 
     public override async Task<GetToppingsResponse> GetToppings(GetToppingsRequest request, ServerCallContext context)
     {
+        var start = Stopwatch.GetTimestamp();
         var toppings = await _toppingData.GetAsync(context.CancellationToken);
 
         var response = new GetToppingsResponse
@@ -33,6 +37,9 @@ public class IngredientsImpl : IngredientsService.IngredientsServiceBase
             }
         };
 
+        var stop = Stopwatch.GetTimestamp();
+        
+        _logger.LogInformation("GetToppings completed in {Duration}", (stop - start).ToString());
         return response;
     }
 
